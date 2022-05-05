a7ea6ca8d0da03b9a2a2ae3aa04cd94b4e95c5e4
Karan Shah <awskaran@amazon.co.uk>
2022-05-05 14:40:51 +0100

    Egress App frontend code

---
+++ src/components/egress_app_frontend/src/App.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import './style/App.css';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react';
+import EgressRequestList from './components/egress/EgressRequestList';
+import awsconfig from './aws-config';
+import NavBar from './components/NavBar/navbar';
+
+Amplify.configure(awsconfig);
+
+function App() {
+    const [user, setUser] = useState();
+    function getUser() {
+        return Auth.currentAuthenticatedUser().then((userData) => userData);
+    }
+
+    useEffect(() => {
+        Hub.listen('auth', ({ payload: { event } }) => {
+            switch (event) {
+                case 'signIn':
+                case 'cognitoHostedUI':
+                    getUser().then((userData) => setUser(userData));
+                    break;
+                case 'signOut':
+                    setUser(null);
+                    break;
+                case 'signIn_failure':
+                case 'cognitoHostedUI_failure':
+                default:
+            }
+        });
+
+        getUser().then((userData) => setUser(userData));
+    }, []);
+
+    return user ? (
+        <div className="App">
+            <NavBar />
+            <EgressRequestList />
+        </div>
+    ) : (
+        <AmplifyAuthenticator>
+            <AmplifySignIn
+                slot="sign-in"
+                hideSignUp="true"
+                headerText="Sign In"
+                // formFields={null}
+                // federated={federated}
+            >
+                {/* <div slot="federated-buttons">
+          <Button color='primary'
+                  variant='contained'
+                  className='sign-in-button'
+                  fullWidth
+                  onClick={() => Auth.federatedSignIn({customProvider: "AWSSSO"})}
+                  >Sign In with College Credentials</Button> 
+          
+        </div> */}
+            </AmplifySignIn>
+        </AmplifyAuthenticator>
+    );
+}
+
+export default App;
---
+++ src/components/egress_app_frontend/src/graphql/queries.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const listRequests = /* GraphQL */ `
+    query ListRequests {
+        listRequests {
+            egress_request_id
+            egress_store_name
+            swb_egress_store_id
+            project_id
+            workspace_id
+            requested_by
+            requested_dt
+            updated_by
+            updated_dt
+            egress_status
+            s3_egress_store_bucket
+            s3_egress_store_bucket_path
+            s3_egress_store_object_list_location
+            s3_swb_object_metadata_version
+            task_token
+            ig_reviewer_1_name
+            ig_reviewer_1_email
+            ig_reviewer_1_dt
+            ig_reviewer_1_reason
+            ig_reviewer_1_decision
+            rit_reviewer_2_name
+            rit_reviewer_2_email
+            rit_reviewer_2_dt
+            rit_reviewer_2_reason
+            rit_reviewer_2_decision
+            current_reviewer_group
+            s3_staging_object_metadata_version
+            download_count
+        }
+    }
+`;
