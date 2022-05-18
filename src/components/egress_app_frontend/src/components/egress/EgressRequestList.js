141a540f2317daff6782c3404fd4b5fd6cc5df4d
Karan Shah <awskaran@amazon.co.uk>
2022-05-18 16:44:41 +0100

    Adding AWS copyright statement to workspace backup and egress app frontent

--- src/components/egress_app_frontend/src/components/egress/EgressRequestList.js
+++ src/components/egress_app_frontend/src/components/egress/EgressRequestList.js
@@ -1,3 +1,9 @@
+// (c) 2022 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
+//
+// This AWS Content is provided subject to the terms of the AWS Customer Agreement
+// available at http://aws.amazon.com/agreement or other written agreement between
+// Customer and either Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
+
 import React, { useState, useEffect } from 'react';
 import '../../style/App.css';
 import {
--- src/components/workspace_backup/app.py
+++ src/components/workspace_backup/app.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+
+# (c) 2022 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
+# This AWS Content is provided subject to the terms of the AWS Customer
+# Agreement available at https://aws.amazon.com/agreement or other written
+# agreement between Customer and Amazon Web Services, Inc.
+
 import aws_cdk as cdk
 from aws_cdk import Aspects
 from cdk_nag import AwsSolutionsChecks
